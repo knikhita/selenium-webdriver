b57ed263a429038a53d726b16cbb5912cc288fb0
1nicks52@gmail.com <nikhita@Nikhitas-MacBook-Pro.local>
2015-12-09 01:51:35 +0530

    added SWD1.rb

---
+++ SWD1.rb
@@ -0,0 +1,17 @@
+require 'rubygems'
+require 'selenium-webdriver'
+
+driver = Selenium::WebDriver.for :firefox
+
+wait = Selenium::WebDriver::Wait.new(:timeout => 30)
+
+driver.navigate.to "file:///Users/nikhita/Desktop/RUBYTESTS/Practices/TEST.html"
+driver.get "file:///Users/nikhita/Desktop/RUBYTESTS/Practices/TEST.html"
+
+#wait = Selenium::WebDriver::Wait.new(:timeout => 30)
+puts "Test Passed" if wait.until{ driver.find_element(:id => '1')}
+#element = element.displayed?
+
+
+#element = wait.until{ driver.find_element(:id => '1')}
+#driver.quit
