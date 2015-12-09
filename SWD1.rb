fe457fdd772d5259f3f5f291487a417000dce0ce
nicks52 <1nicks52@gmail.com>
2015-12-09 12:37:08 +0530

    ID get changed from 1 to 2

--- SWD1.rb
+++ SWD1.rb
@@ -9,7 +9,7 @@ driver.navigate.to "file:///Users/nikhita/Desktop/RUBYTESTS/Practices/TEST.html"
 driver.get "file:///Users/nikhita/Desktop/RUBYTESTS/Practices/TEST.html"
 
 #wait = Selenium::WebDriver::Wait.new(:timeout => 30)
-puts "Test Passed" if wait.until{ driver.find_element(:id => '1')}
+puts "Test Passed" if wait.until{ driver.find_element(:id => '2')}
 #element = element.displayed?
 
 
