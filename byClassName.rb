87fcd0bd1978872cedb12770971bceb1c1d964e2
1nicks52@gmail.com <nikhita@Nikhitas-MacBook-Pro.local>
2015-12-11 12:20:33 +0530

    Locate element by Class Name

---
+++ byClassName.rb
@@ -0,0 +1,19 @@
+require 'rubygems'
+require 'selenium-webdriver'
+
+driver = Selenium::WebDriver.for :firefox
+
+driver.navigate.to "https://housing.com/in/buy/mumbai/mumbai"
+
+#wait = Selenium::WebDriver::Wait.new(:timeout,"20")
+
+driver.get "https://housing.com/in/buy/mumbai/andheri_east"
+
+#element = driver.find_element(:class_name,"buy-service")
+element = driver.find_element(:class,"buy-service")
+
+puts "Test passed for Class Name Locator of #{driver.title}"
+driver.quit
+
+
+
