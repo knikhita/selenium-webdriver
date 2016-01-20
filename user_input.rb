3e7ee577074d30d02c7c17941fe1ee390230c878
1nicks52@gmail.com <nikhita@Nikhitas-MacBook-Pro.local>
2016-01-20 12:05:41 +0530

    Basic ruby webdriver program

---
+++ user_input.rb
@@ -0,0 +1,15 @@
+require 'rubygems'
+require 'selenium-webdriver'
+ 
+browser = Selenium::WebDriver.for :ff
+browser.get "http://www.google.com"
+input = browser.find_element(:id,'gbqfq')
+input.send_keys('aspyct')
+button = browser.find_element(:id,'gbqfb')
+button.click
+
+link = browser.find_element(:link,'Aspyct.org')
+link.click
+
+browser.title
+browser.find_element(:xpath,'\header\hgroup\h1').text
