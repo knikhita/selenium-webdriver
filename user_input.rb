15229b2d65ad2d327c5d0a2f1a86be6507b944b2
1nicks52@gmail.com <nikhita@Nikhitas-MacBook-Pro.local>
2016-01-22 14:45:18 +0530

    Additional code inserted

--- user_input.rb
+++ user_input.rb
@@ -2,15 +2,19 @@ require 'rubygems'
 require 'selenium-webdriver'
  
 browser = Selenium::WebDriver.for :ff
+
 browser.get "http://www.google.com"
-input = browser.find_element(:id,'gbqfq')
+input = browser.find_element(:name,'q')
 input.send_keys('aspyct')
-button = browser.find_element(:id,'gbqfb')
+browser.manage.timeouts.implicit_wait = 10 # seconds
+button = browser.find_element(:name,'btnG')
 button.click
 
-link = browser.find_element(:link,'Aspyct.org')
+#wait = Selenium::WebDriver::Wait.new(:timeout,'10')
+
+link = browser.find_element(:link_text,'Aspyct.org')
 link.click
 
 browser.title
-browser.find_element(:xpath,'\header\hgroup\h1').text
+browser.find_element(:xpath,'//h1').text
 browser.quit()
