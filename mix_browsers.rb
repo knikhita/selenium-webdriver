8b53812e014464820159ddbdeef20a2cb5fe2c8c
1nicks52@gmail.com <nikhita@Nikhitas-MacBook-Pro.local>
2016-01-22 15:36:13 +0530

    Perform cross platform automation for loading same site

---
+++ mix_browsers.rb
@@ -0,0 +1,24 @@
+require 'selenium-webdriver'
+
+describe "Selenium Recipes - Start different browsers" do
+it "Start Chrome" do
+driver = Selenium::WebDriver.for(:chrome)
+driver.navigate.to("http://travel.agileway.net")
+sleep 1
+driver.quit
+end
+it "Start FireFox" do
+driver = Selenium::WebDriver.for(:firefox)
+driver.navigate.to("http://travel.agileway.net")
+expect(driver.title).to eq("Agile Travel")
+sleep 1
+driver.quit
+end
+it "Start IE" do
+driver = Selenium::WebDriver.for(:ie)
+driver.get("http://travel.agileway.net")
+sleep 1
+expect(driver.page_source).to include("User Name")
+driver.quit
+end
+end
