628f9c7bff70e94463ec6803159d3e6dbda88ef7
1nicks52 <mangal25>
2016-02-16 14:20:37 +0530

    Implicit wait added (extend upto 100ms) Please enter the commit message for your changes. Lines starting

--- browsers_ops.rb
+++ browsers_ops.rb
@@ -6,10 +6,11 @@ driver.get "http://google.com"
 driver.find_element(name: "q")
 driver.find_element(name: "q").send_keys "housing.com"
 driver.navigate.to "https://housing.com"
+driver.manage.timeouts.implicit_wait = 50 
 driver.get "https://housing.com/in/buy/mumbai/andheri_east"
-driver.manage.timeouts.implicit_wait = 10 
+driver.manage.timeouts.implicit_wait = 100 
 driver.navigate().back();
-driver.manage.timeouts.implicit_wait = 10 
+driver.manage.timeouts.implicit_wait = 100 
 #wait = Selenium::WebDriver::Wait.new(:timeout => 10) #explicit wait
 driver.navigate().forward();
 puts "Test Case passed browser's operations"
