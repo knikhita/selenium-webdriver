0351be2b4f7fa1450d488a878e059e10d9a73dea
1nicks52 <mangal25>
2016-02-16 14:15:06 +0530

    Browsers back() & forward() functions added

---
+++ browsers_ops.rb
@@ -0,0 +1,18 @@
+require 'rubygems'
+require 'selenium-webdriver'
+
+driver = Selenium::WebDriver.for :ff
+driver.get "http://google.com"
+driver.find_element(name: "q")
+driver.find_element(name: "q").send_keys "housing.com"
+driver.navigate.to "https://housing.com"
+driver.get "https://housing.com/in/buy/mumbai/andheri_east"
+driver.manage.timeouts.implicit_wait = 10 
+driver.navigate().back();
+driver.manage.timeouts.implicit_wait = 10 
+#wait = Selenium::WebDriver::Wait.new(:timeout => 10) #explicit wait
+driver.navigate().forward();
+puts "Test Case passed browser's operations"
+
+driver.quit()
+
